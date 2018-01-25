1dae505263ea99747b0a825edcf0473262146968
Sam Chai <sam.chai0501@gmail.com>
2018-01-25 16:23:50 -0500

    Displays text above the object when clicking on the object

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'aframe'
 import { Entity, Scene } from 'aframe-react'
+import { setAttributes } from './utils'
 import 'babel-polyfill'
 import 'aframe-particle-system-component'
 import 'aframe-physics-system'
@@ -16,6 +17,20 @@ class App extends React.Component {
   }
 
   _handleClick() {
+    let sceneEl = document.getElementById('scene')
+    let markerEl = document.getElementById('box')
+    let text = document.getElementById('text')
+    if (text) text.parentNode.removeChild(text)
+    let position = markerEl.object3D.getWorldPosition()
+    let newEl = document.createElement('a-text')
+    position.y = position.y + 2
+    setAttributes(newEl, {
+      color: 'black',
+      value: 'Hi',
+      id: 'text',
+      position: position
+    })
+    sceneEl.appendChild(newEl)
     this.setState({
       colorIndex: (this.state.colorIndex + 1) % COLORS.length
     })
@@ -23,11 +38,12 @@ class App extends React.Component {
 
   render() {
     return (
-      <Scene physics="debug: true" inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js">
-        <Entity primitive="a-camera">
+      <Scene id="scene" physics="debug: true" inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js">
+        <Entity id="camera" primitive="a-camera">
           <Entity primitive="a-cursor" />
         </Entity>
         <Entity
+          id="floor"
           static-body
           primitive="a-box"
           depth="50"
@@ -36,8 +52,10 @@ class App extends React.Component {
           color="#2E3837"
           shader="flat"
           physics-body="mass: 0; boundingBox: 50 0.1 50"
-          position="0 0 -10" />
+          position="0 0 -10"
+        />
         <Entity
+          id="box"
           geometry={{primitive: 'box'}}
           dynamic-body
           material={{color: COLORS[this.state.colorIndex] }}
