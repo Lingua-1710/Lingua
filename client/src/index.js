d5023fd9a24d9d087fad421272c9bf4488ee033f
Sam Chai <sam.chai0501@gmail.com>
2018-01-25 14:48:53 -0500

    added onclick to box object with cursor, added eslint file

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'aframe';
-import { Entity, Scene } from 'aframe-react';
-import 'babel-polyfill';
-import 'aframe-particle-system-component';
-import 'aframe-physics-system';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'aframe'
+import { Entity, Scene } from 'aframe-react'
+import 'babel-polyfill'
+import 'aframe-particle-system-component'
+import 'aframe-physics-system'
+
+const COLORS = ['#D92B6A', '#9564F2', '#FFCF59']
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      colorIndex: 0,
+      spherePosition: { x: 0.0, y: 4, z: -10.0 }
+    }
+  }
+
+  _handleClick() {
+    this.setState({
+      colorIndex: (this.state.colorIndex + 1) % COLORS.length
+    })
+  }
+
   render() {
     return (
       <Scene physics="debug: true" inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js">
-        <Entity static-body geometry="primitive: box; depth: 50; height: 0.1; width: 50"
-                material="color: #2E3837; shader: flat"
-                physics-body="mass: 0; boundingBox: 50 0.1 50" position="0 0 -10" />
-        <Entity dynamic-body geometry={{ primitive: 'box' }} material={{ color: 'red' }} position={{ x: 0, y: 2, z: -5 }} />
+        <Entity primitive="a-camera" look-controls>
+          <Entity
+            primitive="a-cursor"
+            cursor={{ fuse: false }}
+            material={{ color: 'white', shader: 'flat', opacity: 0.75 }}
+            geometry={{ radiusInner: 0.005, radiusOuter: 0.007 }}
+            event-set__1={{
+              _event: 'mouseenter',
+              scale: { x: 1.4, y: 1.4, z: 1.4 }
+            }}
+            event-set__2={{
+              _event: 'mouseleave',
+              scale: { x: 1, y: 1, z: 1 }
+            }}
+            raycaster={{
+              objects: '.clickable'
+            }}
+          />
+        </Entity>
+        <Entity
+          static-body primitive="a-box" depth="50" height="0.1" width="50"
+          material="color: #2E3837; shader: flat"
+          physics-body="mass: 0; boundingBox: 50 0.1 50"
+          position="0 0 -10" />
+        <Entity
+          class="clickable"
+          dynamic-body primitive="a-box"
+          material={{ color: COLORS[this.state.colorIndex] }}
+          position={{ x: 0, y: 2, z: -5 }}
+          events={{
+            click: this._handleClick.bind(this)
+          }}
+        />
         <Entity particle-system={{ preset: 'snow' }} />
         <Entity light={{ type: 'point' }} />
         <Entity text={{ value: 'Hello, WebVR!' }} />
@@ -20,4 +65,4 @@ class App extends React.Component {
     )
   }
 }
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
