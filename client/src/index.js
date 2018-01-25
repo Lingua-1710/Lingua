c2d0fd8b4c6bcebd3c5f9e8dc5fa7423bd6c65ed
Sam Chai <sam.chai0501@gmail.com>
2018-01-25 12:14:19 -0500

    add floor

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,11 @@ import 'aframe-physics-system';
 class App extends React.Component {
   render() {
     return (
-      <Scene>
-        <Entity geometry={{ primitive: 'box' }} material={{ color: 'red' }} position={{ x: 0, y: 0, z: -5 }} />
+      <Scene physics="debug: true" inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js">
+        <Entity static-body geometry="primitive: box; depth: 50; height: 0.1; width: 50"
+                material="color: #2E3837; shader: flat"
+                physics-body="mass: 0; boundingBox: 50 0.1 50" position="0 0 -10" />
+        <Entity dynamic-body geometry={{ primitive: 'box' }} material={{ color: 'red' }} position={{ x: 0, y: 2, z: -5 }} />
         <Entity particle-system={{ preset: 'snow' }} />
         <Entity light={{ type: 'point' }} />
         <Entity text={{ value: 'Hello, WebVR!' }} />
