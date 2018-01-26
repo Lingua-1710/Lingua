71ceeb5330a90075cca683e40335d5f0ba47a8b9
Sam Chai <sam.chai0501@gmail.com>
2018-01-26 14:36:21 -0500

    heroku files

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const router = require('./router')
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname,'..','public')))
 
 app.use('/', router)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`listening on port 5000!`)
 })
 
