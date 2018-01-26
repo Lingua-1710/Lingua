717bab706bebaf494481836ce109e05999ddaa31
Sam Chai <sam.chai0501@gmail.com>
2018-01-26 12:32:16 -0500

    fix formatting to correspond with linter

--- client/src/index.js
+++ client/src/index.js
@@ -8,45 +8,43 @@ import 'aframe-physics-system'
 import { FirstVendor, Box, Cursor, Floor } from '../components'
 
 class Main extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    
     this.state = {}
   }
-  
-  render(){
-  return (
-    <Scene
-      id="scene"
-      physics="debug: true"
-      inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js"
-      environment={{
-        preset: 'starry',
-        seed: 2,
-        lighting: 'distant',
-        lightPosition: {
-          x: -0.110,
-          y: 1.000,
-          z: 0.330
-        },
-        fog: 0.8,
-        ground: 'hills',
-        groundYScale: 6.31,
-        groundTexture: 'none',
-        groundColor: '#2c441f',
-        grid: 'none'
-      }}
-    >
-      <FirstVendor />
-      <Cursor />
-      <Box />
-      <Floor />
-    </Scene>
-  )
- }
-};
 
+  render() {
+    return (
+      <Scene
+        id="scene"
+        physics="debug: true"
+        inspector="url: https://aframe.io/releases/0.3.0/aframe-inspector.min.js"
+        environment={{
+          preset: 'starry',
+          seed: 2,
+          lighting: 'distant',
+          lightPosition: {
+            x: -0.110,
+            y: 1.000,
+            z: 0.330
+          },
+          fog: 0.8,
+          ground: 'hills',
+          groundYScale: 6.31,
+          groundTexture: 'none',
+          groundColor: '#2c441f',
+          grid: 'none'
+        }}
+      >
+        <FirstVendor />
+        <Cursor />
+        <Box />
+        <Floor />
+      </Scene>
+    )
+  }
+}
 
-ReactDOM.render(<Main />, document.getElementById('main'));
+ReactDOM.render(<Main />, document.getElementById('main'))
 
 
