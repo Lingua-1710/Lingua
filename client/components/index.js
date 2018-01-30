b5290ea26bc1fc0b17656d4c0807e3460610e183
leighembee <leigh.blechman@gmail.com>
2018-01-30 16:02:24 -0500

    Fix: refactoring dom manipulation

--- client/components/index.js
+++ client/components/index.js
@@ -1,5 +1,7 @@
 export {default as Cursor} from './Cursor'
 export {default as Floor} from './Floor'
 export { default as FirstVendor } from './FirstVendor'
-export { default as FirstVendorStoreFront } from './FirstVendorStoreFront'
+export { default as FirstVendorStoreFront } from
+  './FirstVendorStoreFront'
+export { default as PromptText } from './PromptText'
 
