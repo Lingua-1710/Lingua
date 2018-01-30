28e94c1124d9a85bc97c8420395994dbbf009d55
Sam Chai <sam.chai0501@gmail.com>
2018-01-30 15:44:49 -0500

    display a panel home-screen

--- client/components/HomeScreen.js
+++ client/components/HomeScreen.js
@@ -1,58 +1,57 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import 'aframe'
 import { Entity } from 'aframe-react'
-import 'babel-polyfill'
-import { setAttributes, COLORS, QUESTIONS, fetchRandomQuestion } from './utils'
+import { getGameState } from '../store'
 
-class Box extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      colorIndex: 0
+const HomeScreen = (props) => {
+  // handleBoxClick() {
+  //   const sceneEl = document.getElementById('scene')
+  //   const markerEl = document.getElementById('box')
+  //   let text = document.getElementById('text')
+  //   let prevQuestion = ''
+  //   if (text) {
+  //     prevQuestion = text.getAttribute('value')
+  //     //remove text element if exists already
+  //     text.parentNode.removeChild(text)
+  //   }
+  //   let position = markerEl.object3D.getWorldPosition()
+  //   let newEl = document.createElement('a-text')
+  //   position.y = position.y + 2
+  //   let question = fetchRandomQuestion(QUESTIONS, prevQuestion)
+  //   setAttributes(newEl, {
+  //     color: 'black',
+  //     value: question,
+  //     id: 'text',
+  //     position: position,
+  //     align: 'center'
+  //   })
+  //   sceneEl.appendChild(newEl)
+  //   this.setState({
+  //     colorIndex: (this.state.colorIndex + 1) % COLORS.length
+  //   })
+  // }
+
+  // const gameState = props.gameState
+  return (
+    <Entity
+      id="plane"
+      primitive="a-plane"
+      height="6"
+      width="6"
+      position="0 3 -4"
+    />
+  )
+}
+
+const mapStateToProps = ({ gameState }) => ({ gameState })
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setGameState(gameState) {
+      dispatch(getGameState(gameState))
     }
-  }
-
-  handleBoxClick() {
-    const sceneEl = document.getElementById('scene')
-    const markerEl = document.getElementById('box')
-    let text = document.getElementById('text')
-    let prevQuestion = ''
-    if (text) {
-      prevQuestion = text.getAttribute('value')
-      //remove text element if exists already
-      text.parentNode.removeChild(text)
-    }
-    let position = markerEl.object3D.getWorldPosition()
-    let newEl = document.createElement('a-text')
-    position.y = position.y + 2
-    let question = fetchRandomQuestion(QUESTIONS, prevQuestion)
-    setAttributes(newEl, {
-      color: 'black',
-      value: question,
-      id: 'text',
-      position: position,
-      align: 'center'
-    })
-    sceneEl.appendChild(newEl)
-    this.setState({
-      colorIndex: (this.state.colorIndex + 1) % COLORS.length
-    })
-  }
-
-  render() {
-    return (
-      <Entity
-        id="box"
-        geometry={{primitive: 'box'}}
-        dynamic-body
-        material={{color: COLORS[this.state.colorIndex] }}
-        position={{ x: 0, y: 2, z: -5 }}
-        events={{
-          click: this.handleBoxClick.bind(this)
-        }}
-      />
-    )
   }
 }
 
-export default Box
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen)
