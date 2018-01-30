77f9eba2a611f7bbf8376ab86c7861452a073a28
Sam Chai <sam.chai0501@gmail.com>
2018-01-30 18:38:20 -0500

    add logic to allow moving only during game and disallow looking during loading

--- client/components/Player.js
+++ client/components/Player.js
@@ -5,9 +5,10 @@ import { Entity } from 'aframe-react'
 import { Cursor } from './index'
 
 const Player = (props) => {
-  const move = props.gameState !== 'home-screen'
+  const move = props.gameState === 'game'
+  const look = props.gameState !== 'loading'
   return (
-    <Entity primitive="a-camera" wasd-controls-enabled={move} >
+    <Entity primitive="a-camera" look-controls-enabled={look} wasd-controls-enabled={move} >
       <Cursor />
     </Entity>
   )
