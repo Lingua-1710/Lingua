cd6db3e8a8951cfd14ea5f6bb51c3f3d02c615b3
Sam Chai <sam.chai0501@gmail.com>
2018-01-31 12:18:48 -0500

    add EnterScene test, moved tests folder

---
+++ client/components/tests/EnterScene.spec.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import chai from 'chai'
+import sinon from 'sinon'
+import chaiEnzyme from 'chai-enzyme'
+import sinonChai from 'sinon-chai'
+import { EnterScene } from '../index'
+
+chai.use(chaiEnzyme())
+chai.should()
+chai.use(sinonChai)
+
+describe ('The EnterScene component', () => {
+  let Enter, clickSpy
+  beforeEach('Create component and onChange spy', () => {
+    clickSpy = sinon.spy()
+    Enter = chaiEnzyme.shallow(<EnterScene onClick={clickSpy} />)
+  })
+
+  it('calls click fn', () => {
+    Enter.simulate('click', {})
+    expect(clickSpy.called).to.be.true
+  })
+})
