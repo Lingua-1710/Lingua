35559a1553b1f8116e8919e23b77dd3d67fa649f
Sam Chai <sam.chai0501@gmail.com>
2018-01-31 23:10:47 -0500

    add testing for EnterScene component

--- client/components/tests/EnterScene.spec.js
+++ client/components/tests/EnterScene.spec.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
-import store from '../../store'
 import { EnterScene }  from '../EnterScene'
+import { Entity } from 'aframe-react'
 import enzyme, { shallow } from 'enzyme'
 import sinon from 'sinon'
 
@@ -9,25 +8,38 @@ import Adapter from 'enzyme-adapter-react-16'
 
 enzyme.configure({ adapter: new Adapter() })
 
-describe('Component: EnterScene', () => {
-  it('renders <a-plane>', () => {
-    const tree = renderer.create(
-      <EnterScene store={store} />
-    ).toJSON()
-    expect(tree.type).toBe('a-plane')
+describe("EnterScene", () => {
+  let shallowEnterScene
+  const enterScene = () => {
+    if (!shallowEnterScene) {
+      shallowEnterScene = shallow(
+        <EnterScene />
+      )
+    }
+    return shallowEnterScene
+  }
+
+  beforeEach(() => {
+    shallowEnterScene = undefined
   })
 
-  it('renders <a-plane> with id', () => {
-    const tree = renderer.create(
-      <EnterScene store={store} id="enter-scene-plane" />
-    ).toJSON()
-    expect(tree.props.id).toBe('enter-scene-plane')
-  })
+  describe('Component: EnterScene', () => {
+    it('renders an Entity', () => {
+      const entities = enterScene().find(Entity)
+      expect(entities.length).toBeGreaterThan(0)
+    })
 
-  it('should be able to be clicked', () => {
-    const clickSpy = sinon.spy()
-    const component = shallow(<EnterScene setGameState={clickSpy} />)
-    component.find('#enter-scene-plane').prop("events").click()
-    expect(clickSpy.called).toBe(true)
+    it('contains everything else that gets rendered', () => {
+      const entities = enterScene().find(Entity)
+      const wrappingEntity = entities.first()
+      expect(wrappingEntity.children()).toEqual(enterScene().children())
+    })
+
+    it('should be able to be clicked', () => {
+      const clickSpy = sinon.spy()
+      const component = shallow(<EnterScene setGameState={clickSpy} />)
+      component.find('#enter-scene-plane').prop("events").click()
+      expect(clickSpy.called).toBe(true)
+    })
   })
 })
