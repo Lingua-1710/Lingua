110efa61fee793708ed42ed8545f3861aac94fd5
Sam Chai <sam.chai0501@gmail.com>
2018-02-05 13:06:16 -0500

    define relations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
-const Item = require('./item')
+const Quest = require('./quest')
+const Character = require('./character')
+const promptResponse = require('./promptResponse')
 const Language = require('./language')
 const Prompt = require('./prompt')
 const Scene = require('./scene')
@@ -7,16 +9,26 @@ const Response = require('./response')
 
 User.belongsToMany(Language, { through: 'user_languages' })
 Language.belongsToMany(User, { through: 'user_languages' })
+
 User.belongsToMany(Scene, { through: 'user_scenes' })
 Scene.belongsToMany(User, { through: 'user_scenes' })
-Item.belongsToMany(Prompt, { through: 'prompt_items' })
-Prompt.belongsToMany(Item, { through: 'prompt_items' })
-Item.belongsToMany(Scene, { through: 'scene_items' })
-Scene.belongsToMany(Item, { through: 'scene_items' })
-Prompt.hasMany(Response)
+
+Scene.belongsToMany(Quest, { through: 'scene_quests' })
+Quest.belongsToMany(Scene, { through: 'scene_quests' })
+
+Scene.belongsToMany(Character, { through: 'scene_characters' })
+Character.belongsToMany(Scene, { through: 'scene_characters' })
+
+Character.belongsToMany(Prompt, { through: 'character_prompts' })
+Prompt.belongsToMany(Character, { through: 'character_prompts' })
+
+Prompt.belongsToMany(Response, { through: promptResponse })
+Response.belongsToMany(Prompt, { through: promptResponse })
 
 module.exports = {
-  Item,
+  Quest,
+  Character,
+  promptResponse,
   Language,
   Prompt,
   Scene,
