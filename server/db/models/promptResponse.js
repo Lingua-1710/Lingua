0815f5f7f3a53a9de52b85a990eed56b50f22293
Sam Chai <sam.chai0501@gmail.com>
2018-02-05 13:13:36 -0500

    add primary key ID to the prompt_responses join table

--- server/db/models/promptResponse.js
+++ server/db/models/promptResponse.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const PromptResponse = db.define('prompt_responses', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   nextPromptId: {
     type: Sequelize.INTEGER,
     allowNull: false
