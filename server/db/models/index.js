e3398ac9b8c3d310b0cd9fbb0a50add1e2bca142
Sam Chai <sam.chai0501@gmail.com>
2018-02-06 09:40:29 -0500

    delete characters/character_prompts

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,5 @@
 const Quest = require('./quest')
 const PromptResponse = require('./promptResponse')
-const CharacterPrompt = require('./characterPrompt')
 const Language = require('./language')
 const Prompt = require('./prompt')
 const Scene = require('./scene')
@@ -16,22 +15,12 @@ Scene.belongsToMany(User, { through: 'user_scenes' })
 Scene.belongsToMany(Quest, { through: 'scene_quests' })
 Quest.belongsToMany(Scene, { through: 'scene_quests' })
 
-<<<<<<< HEAD
-Scene.belongsToMany(Character, { through: 'scene_characters' })
-Character.belongsToMany(Scene, { through: 'scene_characters' })
-
-Prompt.belongsToMany(Character, { through: CharacterPrompt })
-Character.belongsToMany(Prompt, { through: CharacterPrompt })
-
-=======
->>>>>>> c5a6e8cf4e29803aa3b6e35e297e7bb18da7d1d5
 Prompt.belongsToMany(Response, { through: PromptResponse })
 Response.belongsToMany(Prompt, { through: PromptResponse })
 
 module.exports = {
   Quest,
   PromptResponse,
-  CharacterPrompt,
   Language,
   Prompt,
   Scene,
