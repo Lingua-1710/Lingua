c4ee8194c827f7f2bca6590fa2e32df9793c5be4
Sam Chai <sam.chai0501@gmail.com>
2018-02-07 16:42:22 -0500

    add LittleDonut

---
+++ client/components/items/LittleDonut.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import 'aframe'
+import { Entity } from 'aframe-react'
+import 'babel-polyfill'
+
+const LittleDonut = () => {
+  return (
+    <Entity
+      id="little-donut"
+    >
+      <Entity>
+        <a-asset-item
+          id="little-donut-obj"
+          src="models/items/littledonut/model.obj" />
+        <a-asset-item
+          id="little-donut-mtl"
+          src="models/items/littledonut/materials.mtl" />
+      </Entity>
+      <a-obj-model
+        id="little-donut"
+        src="#little-donut-obj"
+        mtl="#little-donut-mtl"
+        position="0 1.5 0"
+        rotation="0 0 0"
+      />
+      <Entity
+        primitive="a-light"
+        type="directional"
+        color="#FFF"
+        intensity={1}
+        position={{ x: 2.5, y: 0.0, z: 0.0 }}
+      />
+    </Entity>
+  )
+}
+
+export default LittleDonut
