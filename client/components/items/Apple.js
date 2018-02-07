326a2059601f5ef68535a1fab80e9105867daf0a
leighembee <leigh.blechman@gmail.com>
2018-02-07 12:45:15 -0500

    Feat: first reward set

---
+++ client/components/items/Apple.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import 'aframe'
+import { Entity } from 'aframe-react'
+import 'babel-polyfill'
+
+const Apple = () => {
+  return (
+    <Entity
+      id="first-vendor"
+    >
+      <Entity>
+        <a-asset-item
+          id="apple-obj"
+          src="models/items/apple/model.obj" />
+        <a-asset-item
+          id="octo-mtl"
+          src="models/items/apple/materials.mtl" />
+      </Entity>
+      <a-obj-model
+        id="apple"
+        src="#apple-obj"
+        mtl="#apple-mtl"
+        position="0 0 0"
+        rotation="0 0 0"
+      />
+      <Entity
+        primitive="a-light"
+        type="directional"
+        color="#FFF"
+        intensity={1}
+        position={{ x: 2.5, y: 0.0, z: 0.0 }}
+      />
+    </Entity>
+  )
+}
+
+export default Apple
